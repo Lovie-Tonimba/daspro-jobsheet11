6ed9f1c7c8225761d457eed41103109e6b9f892a
Lovie-Tonimba <jechonialovie@gmail.com>
2024-12-03 19:28:48 +0700

    modify no 2

--- Gratitude_244107060101.java
+++ Gratitude_244107060101.java
@@ -1,16 +1,15 @@
 public class Gratitude_244107060101 {
-    public static void sayThankyou(){
+    public static void main(String[] args) {
         System.out.println("Thank you for being the best teacher in the world.\n"
         + "You inspired in me a love for learning and made me feel like I could ask you anything.");
-    }
-    public static void main(String[] args) {
-        sayThankyou();
-        String expression = "Thankyou...wish you all the best!!";
-        sayAdditionalGreetings(expression);
-    }
 
-    //EXPERIMENT 2
-    public static void sayAdditionalGreetings(String greeting){
-     System.out.println(greeting);   
+        
+        //EXPERIMENT 2
+    //     String expression = "Thankyou...wish you all the best!!";
+    //     sayAdditionalGreetings(expression);
+    // }
+    // public static void sayAdditionalGreetings(String greeting){
+    //  System.out.println(greeting);   
+    // }
     }
 }
