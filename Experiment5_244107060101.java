8d89fe9773c437c972df68e39a9dc2d3d348b151
Lovie-Tonimba <jechonialovie@gmail.com>
2024-12-05 11:28:21 +0700

    question no.3

--- Experiment5_244107060101.java
+++ Experiment5_244107060101.java
@@ -8,9 +8,18 @@ public class Experiment5_244107060101 {
         }
         System.out.println();
     }
+    static void example(Object...exm){
+        System.out.println("Example using Object");
+        System.out.println("Number of object: " + exm.length);
+        for(Object i: exm){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         show("Programming Fundamentals 2024", 100, 200);
         show("Information Technology ", 1, 2, 3, 4, 5);
         show("Informatics");
+        example(2, "book", 4, "novel", 5, "magazine");
     }
 }
